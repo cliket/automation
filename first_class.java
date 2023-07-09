bda029b0afd7481aa0fb2e3852ef593a4a7310e6
cliket <cliket.tm@gmail.com>
2023-07-10 00:12:40 +0300

    3-th commit

--- first_class.java
+++ first_class.java
@@ -1,6 +1,6 @@
 public class first_class {
     public static void main (String[] args) {
         //generate simple output
-        System.out.println("Hello, Mothefuckers!");
+        System.out.print("Hello, Mothefuckers!\nHow are ypu doing?");
     }
 }
